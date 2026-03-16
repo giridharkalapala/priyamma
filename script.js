25396e40ed46cf8e447df6a8b2836ca7aa2eeaa5
Giridhar <giridharkalapala42@gmaill.com>
2026-03-17 03:30:57 +0530

    Initial commit

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.addEventListener('mousemove', (e) => {
 });
 
 // Typing effect for greeting
-const greetingText = "Hey You Know What! You're the most adorable human i ever met! 💖";
+const greetingText = "You Know What! You're the most adorable human i ever met! 💖";
 const greetingElement = document.querySelector('.greeting');
 let charIndex = 0;
 
